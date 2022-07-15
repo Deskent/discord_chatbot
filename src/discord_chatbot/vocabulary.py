e351f3ec9f6511ab05eb7264555de7e40ff6f932
Deskent <battenetciz@gmail.com>
2022-07-15 12:56:00 +0300

    1.3.4
    fix Vocabulary errors

--- src/discord_chatbot/vocabulary.py
+++ src/discord_chatbot/vocabulary.py
@@ -5,7 +5,7 @@ from discord_chatbot.config import logger, settings
 
 
 class Vocabulary:
-    """Работает с файлом фраз для отправки в дискорд"""
+    """Reads file with text and returns 1 line"""
 
     __VOCABULARY: list = []
     __PATH_TO_FILE: str = settings.VOCABULARY_PATH_FILE
@@ -13,47 +13,45 @@ class Vocabulary:
     @classmethod
     @logger.catch
     def get_message(cls, file_name: str = '') -> str:
-        """Returns random phrase from phrases list"""
+        """Returns phrase from phrases list"""
 
         if file_name:
             cls.__PATH_TO_FILE = file_name
-        vocabulary: list = cls.__get_vocabulary()
         try:
+            vocabulary: list = cls.__get_vocabulary()
             if settings.PHRASE_RANDOM:
                 random.shuffle(vocabulary)
             message_text: str = vocabulary.pop(0).strip()
-            cls.__set_vocabulary(vocabulary)
+            cls.__save_vocabulary(vocabulary)
         except (ValueError, TypeError, FileNotFoundError) as err:
             logger.error(f"Vocabulary ERROR: {err}")
             return ''
-        else:
-            length: int = len(message_text)
-            if length > 60:
-                return cls.get_message()
-            return message_text.lower()
+        return message_text.lower()
 
     @classmethod
     @logger.catch
     def __get_vocabulary(cls) -> list:
         if not cls.__VOCABULARY:
-            cls.__update_vocabulary()
+            cls.__read_vocabulary()
 
         return cls.__VOCABULARY
 
     @classmethod
     @logger.catch
-    def __set_vocabulary(cls, vocabulary: list):
+    def __save_vocabulary(cls, vocabulary: list):
         if not isinstance(vocabulary, list):
             raise TypeError("TypeError: vocabulary is not list ")
         if not vocabulary:
-            cls.__update_vocabulary()
+            cls.__read_vocabulary()
         else:
             cls.__VOCABULARY = vocabulary
 
     @classmethod
     @logger.catch
-    def __update_vocabulary(cls):
+    def __read_vocabulary(cls):
         if not os.path.exists(cls.__PATH_TO_FILE):
-            raise FileNotFoundError(f"{cls.__PATH_TO_FILE} error: ")
+            raise FileNotFoundError(f"{cls.__PATH_TO_FILE} not found.")
         with open(cls.__PATH_TO_FILE, 'r', encoding='utf-8') as f:
-            cls.__VOCABULARY = f.readlines()
+            cls.__VOCABULARY: list = f.readlines()
+            if not cls.__VOCABULARY:
+                raise ValueError(f'{cls.__PATH_TO_FILE} is empty.')
